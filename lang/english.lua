3a9caa14a3aa1cfbd3932bd0204b53a4146705cf
buzz3rs <mmeqaby21@yahoo.com>
2020-08-02 18:29:52 +0000

    Update english.lua

--- lang/english.lua
+++ lang/english.lua
@@ -383,7 +383,7 @@ function en.lang()-- in-table function
 			text8 = 'Click on this link to earn credits.\n after you click this link, you will go to a website where a "Skip Ad" Button should apear after five seconds, after that you will be redirected to me to receive your reward.',
             keyboard = {
                 {
-					{text = 'Watch Ads', callback_data = 'watchads'},
+					--{text = 'Watch Ads', callback_data = 'watchads'},
                     {text = "Gift code", callback_data = 'giftcode'},
 				},
 				{
